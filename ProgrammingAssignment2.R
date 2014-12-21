b41e9dcf02056967e00a2770ac6793925983a4bd
abhijeetKmr1 <abhijeet.kmr1@gmail.com>
2014-12-21 23:56:11 +0530

    Programming Assignment2
    code reviewed and updated

--- ProgrammingAssignment2.R
+++ ProgrammingAssignment2.R
@@ -1,8 +1,13 @@
+## Assumption
+## "X" matrix passed to cacheMatrix is square matrix and  is not singular
+## cache Enviroment is already started with name "cacheEnv"  using command "cacheEnv <- new.env()"
+## For running calll cacheSolve(x) where x is a Square matrix
+
 invertmatrix<- function(x)
 {
 	
 	m <- NULL
-	m <- solve(x) ## calulating the inverse matrix
+	m <- solve(x) ## calculating the inverse matrix
 	m	
 }
 
@@ -11,14 +16,14 @@ cacheSolve <- function(data,...)
 	## checking "invertMatrix" is present in cacheEnviroment or not
 	if(exists("invertMatrix1", envir=cacheEnv))
 	{
-		m <- get("invertMatrix1", envir=cacheEnv) ## retriving from cahe matrix
+		m <- get("invertMatrix1", envir=cacheEnv) ## retriving from cache 
 	}
 	else
 	{		
 		m <- NULL
 	}
 
-	## If value found in cache memory then will enter tyhe if statement
+	## If value found in cache memory then will enter if statement and return the value
 	if(!is.null(m))
 	{
 		message("getting cached data")
@@ -30,7 +35,7 @@ cacheSolve <- function(data,...)
 
 		message("calculating invert matrix")
 		x <- invertmatrix(data)
-		assign("invertMatrix1", x , envir=cacheEnv)
-		x
+		assign("invertMatrix1", x , envir=cacheEnv) ## assing the value in the cache
+		x                                           ## Returning the value
 	}
 }
