6c34823351fecea4b0f5ec44305431c85840e9dc
abhijeetKmr1 <abhijeet.kmr1@gmail.com>
2014-12-21 23:19:03 +0530

    Programming Assignment2
    Few Modification done in the code

--- ProgrammingAssignment2.R
+++ ProgrammingAssignment2.R
@@ -2,21 +2,23 @@ invertmatrix<- function(x)
 {
 	
 	m <- NULL
-	m <- solve(x) 
+	m <- solve(x) ## calulating the inverse matrix
 	m	
 }
 
 cacheSolve <- function(data,...)
 {
+	## checking "invertMatrix" is present in cacheEnviroment or not
 	if(exists("invertMatrix1", envir=cacheEnv))
 	{
-		m <- get("invertMatrix1", envir=cacheEnv)
+		m <- get("invertMatrix1", envir=cacheEnv) ## retriving from cahe matrix
 	}
 	else
 	{		
 		m <- NULL
 	}
 
+	## If value found in cache memory then will enter tyhe if statement
 	if(!is.null(m))
 	{
 		message("getting cached data")
@@ -24,6 +26,8 @@ cacheSolve <- function(data,...)
 	}
 	else
 	{
+		##  calculating the inverse matrix
+
 		message("calculating invert matrix")
 		x <- invertmatrix(data)
 		assign("invertMatrix1", x , envir=cacheEnv)
